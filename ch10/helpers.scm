ac16b9282838f689e8dc4fad8056c816159b3139
Scott Taylor <scott@railsnewbie.com>
2008-12-19 03:15:36 -0500

    * Group helpers

--- ch10/helpers.scm
+++ ch10/helpers.scm
@@ -13,19 +13,21 @@
           (cons b
                 (cons (quote ()))))))
 
-(define text-of second)
-
 (define initial-table
   (lambda (e)
     (car (quote ()))))
+
+(define text-of                        second)
 
 (define arguments-and-body-of-function cdr)
 (define table-of                       first)
 (define arguments-of                   second)
 (define body-of                        third)
+
 (define cond-lines-of                  cdr)
 (define function-of                    car)
 (define arguments-of                   cdr)
+
 (define question-of                    first)
 (define answer-of                      second)
 
